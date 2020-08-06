ec09e8af829529147c9fe5cb69be21b9525d4617
Hardik Sharma <hardik@pop-os.localdomain>
2020-08-07 00:34:04 +0530

    wrote the checker function for links: has_key()

--- li.nk/link.py
+++ li.nk/link.py
@@ -8,19 +8,61 @@
 # uses Python's SQLite3 library for
 # custom link database
 import sqlite3 as sql
+import random
 
-# opens the default database file
-def __init__():
-	conn = sql.connect("links.db")
+# connects the li.nk database
+conn = sql.connect("links.db")
 
 # message for invalid choice
 def invalid():
-	print("Invalid choice.")
+	print("Invalid choice.\n")
 	main()
+
+def has_key(s):
+	global conn
+	# an efficient method to check key presence
+	rows = conn.execute(f"""SELECT *
+													FROM links
+													WHERE key = \"{s}\"""")
+	exists = False
+	for r in rows:
+		exists = True
+		break
+	return exists
 
 # check availability of a link
 def check():
-	conn.execute()
+	global conn
+	key = input("Enter a key to check: li.nk/")
+	if(has_key(key)):
+		print(f"li.nk/{key} is not available.\n")
+	else:
+		print(f"li.nk/{key} is available.\n")
+
+def avail():
+	global conn
+	url = input("Enter your URL: ")
+	# generating random key for user
+	# through an adjective+verb combination
+	nouns = open("nouns.txt", "r")
+	adjectives = open("adjectives.txt", "r")
+	noun = random.choice([i.rstrip().rstrip("\n").lstrip().lstrip("\n") for i in nouns])
+	adjective = random.choice([i.rstrip().rstrip("\n").lstrip().lstrip("\n") for i in adjectives])
+	key = adjective + noun
+	conn.execute(f"""INSERT INTO links (key, url)
+									VALUES ({key}, {url});""")
+	conn.commit()
+	print("Your personal link is created!")
+	print(f"{url} is now: li.nk/{key}")
+
+def dump():
+	global conn
+	dump = conn.execute("""SELECT * FROM links;""")
+	for d in dump:
+		print(d)
+
+def end():
+	raise SystemExit
 
 def main():
 
@@ -34,13 +76,13 @@ def main():
 
 	choice = input("Enter choice: ")
 
-	funclist = 
-	{
+	funclist = {
 		"1": "check()",
 		"2": "avail()",
 		"3": "avail_custom()",
 		"4": "remove()",
-		"5": "end()"
+		"5": "end()",
+		"69": "dump()"
 	}
 
 	eval(funclist.get(choice, "invalid()"))
